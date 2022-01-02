319bc0b60cd8dc9bcc8aafecc467a98580381aa6
rosental <cristiane.rosental@gmail.com>
2022-01-01 21:54:28 -0300

    Routes Web.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/hello-world', function () {
+//    return view('hello-world');
+//});
+//
+////Parametro dinamico
+//Route::get('/hello/{name}', function ($name) {
+//    return 'Hello, ' . $name;
+//});
+//
+////Parametro nao e obrigatorio, passa o ponto de interrogacao no parametro
+//// e no callback da funcao estabelece um valor defult
+//Route::get('/hello/{name?}', function ($name = null) {
+//    return 'Hello, ' . $name;
+//});
+
+//Rota pelo controller passando o nome do metodo
+Route::get('/hello-world', [App\Http\Controllers\HelloWordController::class, 'helloWorld']);
+
+Route::get('/hello/{name?}', [App\Http\Controllers\HelloWordController::class, 'hello']);
+
+//Obedece os verbos HTTP
+//Route::get ou Route::post ou Route::put ou Route::patch ou Route::delete
